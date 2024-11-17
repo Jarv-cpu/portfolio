88b3f7028880d238fb51d7b02729768ce50929f6
Jesus <tvjarv8@gmail.com>
2024-11-17 13:44:16 -0400

    agregando bordes

--- src/components/SkillIcons.jsx
+++ src/components/SkillIcons.jsx
@@ -1,8 +1,10 @@
 export function SkillIcons({ children }) {
     return (
         <article className='bg-gradient-to-t from-slate-900 to-slate-700
-                            flex flex-col items-center text-slate-50 border
-                            border-slate-600 font-bold'>
+                            flex flex-col items-center text-slate-50 border-2
+                            border-slate-700 font-bold
+                            hover:border-slate-500
+                            transition-all duration-500'>
             {children}
         </article>
     )
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,10 +5,10 @@ export function About() {
             <img src="/images/fotoJesus.jpg" alt="Foto de Jesus Romero"
             className='flex relative top-0 animate-flotar w-[200px] h-[200px] rounded-full border-2'/>
             <h1 className='text-[40px] whitespace-nowrap border-r-4 border-white w-[20ch] overflow-hidden animate-typingBlink
-                           max-sm:text-3xl'>Hola, Soy Jesus Romero</h1>
+                           max-sm:text-[25px]'>Hola, Soy Jesus Romero</h1>
 
             <h2>Un Desarrollador Front End Junior</h2>
-            <p className='mt-2 text-lg w-[450px] text-wrap text-center max-sm:w-11/12 max-sm:text-balance'>Tengo 20 años, estudio la carrera de Ing. informatica, me gusta el desarrollo web y aspiro a ser Full Stack</p>
+            <p className='mt-2 text-lg w-[450px] text-wrap text-center max-sm:w-[250px] max-sm:text-balance'>Tengo 20 años, estudio la carrera de Ing. informatica, me gusta el desarrollo web y aspiro a ser Full Stack</p>
         </section>
     )
 }
